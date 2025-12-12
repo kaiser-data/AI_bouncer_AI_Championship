9051cf8fa0a8f656f969a522b7c78453c79ac3fe
MK <martinkaiser.bln@googlemail.com>
2025-12-12 23:44:08 +0100

    Fix: Use correct parameter name 'file' for ElevenLabs STT API

--- src/bouncer/index.ts
+++ src/bouncer/index.ts
@@ -1236,7 +1236,7 @@ app.post('/stt', async (c) => {
     }
     
     const apiFormData = new FormData();
-    apiFormData.append('audio', audioFile);
+    apiFormData.append('file', audioFile);
     apiFormData.append('model_id', 'scribe_v1');
 
     const response = await fetch('https://api.elevenlabs.io/v1/speech-to-text', {
